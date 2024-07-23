a26b99f18f6a8d2790de242999e8a8eb95e52ea5
Sean R Reid <sean@torchcodelab.com>
2024-07-23 16:35:09 -0400

    Refactored main nav, added css module

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,30 +1,29 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../AuthContext";
+
+import styles from "./nav.module.css";
 
 const MainNav = () => {
+  const { isAuth, group } = useAuth();
   return (
-    <nav style={{ maxWidth: 1200, margin: "1rem auto" }}>
-      <ul
-        style={{
-          listStyle: "none",
-          margin: 0,
-          padding: 0,
-          display: "flex",
-          gap: "16px",
-          width: "min-content",
-        }}
-      >
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
+    <nav className={styles.mainNav}>
+      <ul>
+        {isAuth ? (
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </>
+        )}
+        {group && group === "admin" && <li>GROUP: ADMIN</li>}
+        {group && group === "user" && <li>GROUP: USER</li>}
       </ul>
     </nav>
   );
