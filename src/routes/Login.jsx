a3ff01733501cff6601b2da6c91e075152641ee5
Sean R Reid <sean@torchcodelab.com>
2024-07-23 13:59:27 -0400

    Updated login/register logic to return errors and redirect on success

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,37 +1,50 @@
-import { useActionData, Form } from 'react-router-dom';
-import supabase from '../supabase';
+import { useActionData, redirect, json, Form } from "react-router-dom";
+import supabase from "../supabase";
 
 export const action = async ({ request }) => {
-    const formData = await request.formData();
-    const email = formData.get('email');
-    const password = formData.get('password');
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  try {
     const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password
+      email: email,
+      password: password,
     });
-    console.log('DATA', data);
-    console.error('ERROR', error);
-    return data;
+    if (error) {
+      throw error;
+    }
+    if (data.user.id) {
+      return redirect("/");
+    } else {
+      throw new Error("Registration Failed");
+    }
+  } catch (errors) {
+    return json(errors);
+  }
 };
 
 const Register = () => {
-    const data = useActionData();
-    console.log('DATA IN REGISTER:', data);
-    return (
-        <Form method="POST">
-            <label>
-                Email
-                <br />
-                <input type='email' name='email' />
-            </label>
-            <label>
-                Password
-                <br />
-                <input type='password' name='password' />
-            </label>
-            <button type="submit">Login</button>
-        </Form>
-    );
+  const errors = useActionData();
+
+  return (
+    <>
+      {errors && errors.status === 400 && (
+        <div className="error">Invalid Login Credentials</div>
+      )}
+      <Form method="POST">
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Login</button>
+      </Form>
+    </>
+  );
 };
 
 export default Register;
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,53 +1,79 @@
-import { useActionData, Form } from "react-router-dom";
+import { useActionData, redirect, json, Form } from "react-router-dom";
 import supabase from "../supabase";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
-  const { data, error } = await supabase.auth.signUp({
-    email: email,
-    password: password,
-  });
-  console.log("DATA", data);
-  console.error("ERROR", error);
-  return data;
+  const firstName = formData.get("firstName");
+  const lastName = formData.get("lastName");
+  const group = formData.get("group");
+
+  try {
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          first_name: firstName,
+          last_name: lastName,
+          group_membership: group,
+        },
+      },
+    });
+    if (error) {
+      throw error;
+    }
+    if (data.user.id) {
+      return redirect("/");
+    } else {
+      throw new Error("Registration Failed");
+    }
+  } catch (errors) {
+    return json(errors);
+  }
 };
 
 const Register = () => {
-  const data = useActionData();
-  console.log("DATA IN REGISTER:", data);
+  const errors = useActionData();
+
   return (
-    <Form method="POST">
-      <label>
-        First Name
-        <br />
-        <input type="text" name="f_name" />
-      </label>
-      <label>
-        Last Name
-        <br />
-        <input type="text" name="l_name" />
-      </label>
-      <label>
-        Email
-        <br />
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password
-        <br />
-        <input type="password" name="password" />
-      </label>
-      <label>
-        Group
-        <select name="group">
-          <option value="user">User</option>
-          <option value="admin">Administrator</option>
-        </select>
-      </label>
-      <button type="submit">Register</button>
-    </Form>
+    <>
+      {errors && errors.status === 400 && (
+        <div className="error">Unable to Register New User</div>
+      )}
+      <Form method="POST">
+        <label>
+          First Name
+          <br />
+          <input type="text" name="firstName" required />
+        </label>
+        <label>
+          Last Name
+          <br />
+          <input type="text" name="lastName" required />
+        </label>
+        <label>
+          Email
+          <br />
+          <input type="email" name="email" required />
+        </label>
+        <label>
+          Password
+          <br />
+          <input type="password" name="password" required />
+        </label>
+        <label>
+          Group
+          <select name="group" required>
+            <option value="">Select A User Group</option>
+            <option value="user">User</option>
+            <option value="admin">Administrator</option>
+          </select>
+        </label>
+        <button type="submit">Register</button>
+      </Form>
+    </>
   );
 };
 
